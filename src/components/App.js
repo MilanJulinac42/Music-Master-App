6ef3a6b538156c4c0299f0948ef92d8fd6f979e6
Milan <milanjulinac996@gmail.com>
2020-05-19 14:27:26 +0200

    Music master app, you can view 10 songs and play short verion of them

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Artist from "./Artist";
+import Tracks from "./Tracks";
+import Search from "./Search";
+
+const API_ADRESS = "https://spotify-api-wrapper.appspot.com";
+
+class App extends Component {
+  state = { artist: null, tracks: [] };
+
+  componentDidMount() {
+    this.searchArtist("the weeknd");
+  }
+
+  searchArtist = (artistQuery) => {
+    fetch(`${API_ADRESS}/artist/${artistQuery}`)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.artists.total > 0) {
+          const artist = json.artists.items[0];
+          this.setState({ artist });
+          fetch(`${API_ADRESS}/artist/${artist.id}/top-tracks`)
+            .then((response) => response.json())
+            .then((json) => this.setState({ tracks: json.tracks }))
+            .catch((error) => alert(error.message));
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Music Master</h2>
+        <Search searchArtist={this.searchArtist} />
+        <Artist artist={this.state.artist} />
+        <Tracks tracks={this.state.tracks} />
+      </div>
+    );
+  }
+}
+
+export default App;
